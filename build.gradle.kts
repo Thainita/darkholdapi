f77c531afc6668e7fba25cd91b609761ea521f1e
Thainá Rodrigues <rodrigues.thainab@gmail.com>
2021-08-11 22:01:23 -0300

    Inclusion of webscrapper package.

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,6 +20,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	implementation("it.skrape:skrapeit:1.1.5")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
